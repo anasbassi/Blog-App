0b1a4db4c9367744abc7c38e07422a8d4f66ab27
Anas Abubakar <anasabubakar2015@gmail.com>
2022-10-29 07:15:34 +0100

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index create show]
   def index
     @posts = @user.posts
   end
@@ -7,13 +8,33 @@ class PostsController < ApplicationController
     set_post
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
   private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 
   def set_post
     @post = Post.find(params[:id])
   end
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:author_id])
   end
 end
