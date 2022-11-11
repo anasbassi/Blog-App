55d8d1fdb32307f1c45f9170e0376493fdbf693b
Anas Abubakar <anasabubakar2015@gmail.com>
2022-11-11 16:56:41 +0100

    Implement delete action

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :comment_counter
+  after_create :comment_counter
 
   def comment_counter
     post.increment!(:comments_counter)
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   # get "/", to: "users#index"
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, except: [:update, :edit] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
