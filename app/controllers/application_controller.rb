b8ac36cd5d5121f4fd1fddf503a602491f608f11
Anas Abubakar <anasabubakar2015@gmail.com>
2022-11-12 16:30:48 +0100

    Add Api end points

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def json_response(json, status)
+    render json:, status:
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
 end
